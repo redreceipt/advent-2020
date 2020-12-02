b7b1069f64205f19bd43c318f376c2ef83e4ddde
Michael Neeley <micneeley14@gmail.com>
2020-12-02 10:54:04 -0500

    made day1 better

--- day1/solve.py
+++ day1/solve.py
@@ -1,17 +1,21 @@
+def one(nums):
+    for x in nums:
+        for y in nums:
+            if x + y == 2020:
+                return x * y
+
+
+def two(nums):
+    for x in nums:
+        for y in nums:
+            for z in nums:
+                if x + y + z == 2020:
+                    return x * y * z
+
+
 f = open("input.txt")
 nums = [int(num) for num in f.read().split("\n") if num != ""]
 
-found = False
-for x in nums:
-    for y in nums:
-        for z in nums:
-            if x + y + z == 2020:
-                found = True
-                break
-        if found:
-            break
-    if found:
-        break
 
-
-print(x * y * z)
+print(one(nums))
+print(two(nums))
