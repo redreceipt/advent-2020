2b3f10616b148ff55d6c67ee688a4f673a1c14aa
Michael Neeley <micneeley14@gmail.com>
2020-12-08 01:22:06 -0500

    day7

--- day7/solve.py
+++ day7/solve.py
@@ -1,22 +1,26 @@
 def get_rules(lines):
+    def clean_color(color):
+        return color.strip().replace(" bags", "").replace(" bag",
+                                                          "").replace(".", "")
+
     return {
-        parent.strip().replace(" bags", ""): [
-            child.strip().replace(" bags", "").replace(" bag", "").replace(
-                ".", "")[2:] if "no" not in child else None
+        parent.strip().replace(" bags", ""): {
+            clean_color(child)[2:]:
+            clean_color(child)[0] if "no" not in child else 0
             for child in children.split(",")
-        ]
+        }
         for parent, children in [line.split("contain") for line in lines]
     }
 
 
 def one(lines, color):
     rules = get_rules(lines)
-
     ancestors = set()
 
     def getParents(child):
-        parents = set(
-            [parent for parent in rules.keys() if child in rules[parent]])
+        parents = set([
+            parent for parent in rules.keys() if child in rules[parent].keys()
+        ])
         ancestors.update(parents)
         for parent in parents:
             getParents(parent)
@@ -26,8 +30,25 @@ def one(lines, color):
 
 
 def two(lines, color):
-    pass
+    rules = get_rules(lines)
+    num_bags = 0
+
+    def get_children(parent):
+        nonlocal num_bags
+        children = [[child] * int(rules[parent][child])
+                    for child in rules[parent].keys()]
+        for group in children:
+            num_bags += len(group)
+            for child in group:
+                get_children(child)
+
+        return children
+
+    get_children(color)
+    return num_bags
 
 
 f = open("input.txt")
-print(one(f.readlines(), "shiny gold"))
+lines = f.readlines()
+print(one(lines, "shiny gold"))
+print(two(lines, "shiny gold"))
